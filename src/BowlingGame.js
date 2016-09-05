2aacd2e07df9bd7ce52eaedbcd2a341515ca171a
Pram Gurusinga <pgurusinga@babbel.com>
2016-09-05 12:51:57 +0200

    initial commit

---
+++ src/BowlingGame.js
@@ -0,0 +1,79 @@
+'use strict';
+
+class BowlingGame {
+  constructor() {
+    // Default configuration of bowling game
+    this.maxIndexFrame = 2;
+    this.maxFrames = 10;
+    this.maxPinsDown = 10;
+    this.frames = [];
+    this.rolls = [];
+  }
+
+  roll(pins) {
+    if (this.frames.length < this.maxFrames) {
+      this.rolls.push(pins);
+      if (this.isLastIndexOfFrame()) {
+        this.getNextFrame();
+      }
+    }
+  }
+
+  rollRange(attempts, pinsDown) {
+    for (let count = 0; count < attempts; count++) {
+      this.roll(pinsDown);
+    }
+  }
+
+  isLastIndexOfFrame() {
+    return this.rolls.length >= this.maxIndexFrame || this.isStrike(this.rolls);
+  }
+
+  getNextFrame() {
+    if (this.frames.length < this.maxFrames) {
+      this.frames.push(this.rolls);
+    }
+    this.rolls = [];
+  }
+
+  isStrike(roll) {
+    return roll[0] === this.maxPinsDown;
+  }
+
+  isSpare(roll) {
+    return roll[0] + roll[1] === this.maxPinsDown;
+  }
+
+  getTotalScore() {
+    let score = 0;
+    for (let index = 0; index < this.frames.length; index++) {
+      const currentFrame	= this.frames[index];
+      const nextFrame = this.frames[index + 1];
+      if (this.isStrike(currentFrame)) {
+        score += this.getPointsFrom(currentFrame) + this.getStrikeBonus(nextFrame);
+			} else if (this.isSpare(currentFrame)) {
+				score += this.getPointsFrom(currentFrame) + this.getSpareBonus(nextFrame);
+			} else {
+        score += this.getPointsFrom(currentFrame);
+			}
+		}
+    return score;
+  }
+
+  getStrikeBonus(frame) {
+    return frame[0] + frame[1];
+  }
+
+  getSpareBonus(frame) {
+    if (this.rolls.length) return this.rolls[0];
+    return frame[0];
+  }
+
+  getPointsFrom(frame) {
+    return frame.reduce((previous, current) => {
+      return previous + current;
+    });
+  }
+}
+
+export default BowlingGame;
---
+++ test/BowlingGameTest.js
@@ -0,0 +1,72 @@
+import { expect } from 'chai';
+import BowlingGame from '../src/BowlingGame';
+
+describe('Bowling Game', () => {
+  let playerOne;
+
+  beforeEach(() => {
+    playerOne = new BowlingGame();
+  });
+
+  describe('Game player creation', () => {
+    it('creates player', () => {
+      expect(playerOne).to.be.an.instanceof(BowlingGame);
+    });
+  });
+
+  describe('Regular game without getting any pin(s) down', () => {
+    it('should NOT have any pin(s) down and ZERO score', () => {
+      playerOne.rollRange(20, 0);
+      expect(playerOne.getTotalScore()).to.equal(0);
+    });
+  });
+
+  describe('Regular game with one pin down on each frame', () => {
+    it('should have 20 Points', () => {
+      playerOne.rollRange(20, 1);
+      expect(playerOne.getTotalScore()).to.equal(20);
+    });
+  });
+
+  describe('Regular game with spare(s)', () => {
+    it('should validate game with one spare', () => {
+      playerOne.roll(5);
+      playerOne.roll(5);
+      playerOne.roll(2);
+      playerOne.roll(1);
+      playerOne.rollRange(18, 0);
+      expect(playerOne.getTotalScore()).to.equal(15);
+    });
+    it('should validate game with two spares', () => {
+      playerOne.roll(5);
+      playerOne.roll(5);
+      playerOne.roll(2);
+      playerOne.roll(1);
+      playerOne.roll(5);
+      playerOne.roll(5);
+      playerOne.roll(2);
+      playerOne.roll(1);
+      playerOne.rollRange(16, 0);
+      expect(playerOne.getTotalScore()).to.equal(30);
+    });
+  });
+  describe('Regular game with strike(s)', () => {
+    it('should validate game with one strike', () => {
+      playerOne.roll(10);
+      playerOne.roll(2);
+      playerOne.roll(1);
+      playerOne.rollRange(18, 0);
+      expect(playerOne.getTotalScore()).to.equal(16);
+    });
+    it('should validate game with two strikes', () => {
+      playerOne.roll(10);
+      playerOne.roll(2);
+      playerOne.roll(1);
+      playerOne.roll(10);
+      playerOne.roll(2);
+      playerOne.roll(1);
+      playerOne.rollRange(16, 0);
+      expect(playerOne.getTotalScore()).to.equal(32);
+    });
+  });
+});
